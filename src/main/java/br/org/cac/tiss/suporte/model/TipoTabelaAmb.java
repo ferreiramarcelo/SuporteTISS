0a21eb27b8ea2f530cabf258997e2ccff6805f9f
rrm <root@rrm-N61Jv>
2016-06-11 22:48:03 -0300

    conserto autoincremento tipoTabelaAmb

--- src/main/java/br/org/cac/tiss/suporte/model/TipoTabelaAmb.java
+++ src/main/java/br/org/cac/tiss/suporte/model/TipoTabelaAmb.java
@@ -4,6 +4,8 @@ package br.org.cac.tiss.suporte.model;
 import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -16,6 +18,8 @@ import javax.persistence.TemporalType;
 @Table(name = "TIPO_TABELA_AMB")
 public class TipoTabelaAmb implements java.io.Serializable {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int idTipoTabela;
 	private String nome;
 	private String codAns;
