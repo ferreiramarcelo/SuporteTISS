faf29ca6e46056d86f3fe8a927c644c080ef2b2f
Ruben Rodriguez Malvis <RRM@DETIN.CAC>
2016-06-14 17:22:19 -0300

    beans.xml discover all and fisrt dao nao generico

--- src/main/java/br/org/cac/tiss/suporte/bean/UsuariosBean.java
+++ src/main/java/br/org/cac/tiss/suporte/bean/UsuariosBean.java
@@ -1,12 +1,23 @@
 package br.org.cac.tiss.suporte.bean;
 
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.org.cac.tiss.suporte.dao.UsuarioDao;
+import br.org.cac.tiss.suporte.model.SxmlUsuario;
 
 @Named
 @RequestScoped
 public class UsuariosBean {
 	
+	@Inject
+	UsuarioDao dao;
 	
+	public List<SxmlUsuario> listaUsuarios(){
+		return dao.listaTodos();
+	}
 
 }
